ed3b9b74510207b253f8572cd50e2c612e80cecf
Alexandru Florin Bucur <alexandru.f.bucur@avanade.com>
2021-12-20 10:38:56 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import React from 'react';
+
+//import { Button } from 'react-bootstrap'
+//import Button from '@mui/material/Button'
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import IntroductionComponent from './1-Introduction/2-custom-component/IntroductionComponent';
+function App() {
+  // eslint-disable-next-line react/jsx-no-undef
+  return <IntroductionComponent></IntroductionComponent>;
+}
+
+export default App;
